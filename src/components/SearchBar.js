3c65cd84877c90b5c32a1e9769feb81de4d59aa9
David Yu <21711933@student.uwa.edu.au>
2019-02-02 16:52:59 +0800

    add search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class SearchBar extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render () {
+        return (
+            <input placeholder="Find Something" onChange={this.props.handleChange} class="search-bar"></input>
+        )
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,30 @@ import Bio from '../components/Bio'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import { rhythm } from '../utils/typography'
+import SearchBar from '../components/SearchBar'
 
 class BlogIndex extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleSearchBarChange = this.handleSearchBarChange.bind(this)
+
+    this.state = {
+      searchTerm: null
+    }
+  }
+
+  handleSearchBarChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
+    const {searchTerm} = this.state
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -19,8 +37,14 @@ class BlogIndex extends React.Component {
           keywords={[`blog`, `coders for causes`, `javascript`, `react`]}
         />
         <Bio />
+        <SearchBar handleChange={this.handleSearchBarChange}/>
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
+          if (searchTerm && title.toLowerCase().indexOf(searchTerm.toLowerCase()) < 0 && node.excerpt.toLowerCase().indexOf(searchTerm.toLowerCase()) < 0 && node.frontmatter.date.toLowerCase().indexOf(searchTerm.toLowerCase()) < 0) {
+            return (
+              <span></span>
+            )
+          } 
           return (
             <div key={node.fields.slug}>
               <h3
