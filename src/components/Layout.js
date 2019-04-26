73e18fa515859b8153675fcf7a580fea011d341d
David Yu <david.yu@readify.net>
2019-04-26 16:36:24 +0800

    Add light dark mode

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,43 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Toggle from './Toggle'
 
 import { rhythm, scale } from '../utils/typography'
 import Footer from './Footer'
+import sun from '../assets/sun.png'
+import moon from '../assets/moon.png'
 
 class Layout extends React.Component {
+  constructor(props) {
+    super(props)
+
+    let theme = 'light'
+    try {
+      theme = localStorage.theme
+    } catch (err) {}
+
+    this.state = {
+      theme,
+    }
+
+    document.body.className = theme // Not optimal we are doing this here
+  }
+
+  componentDidMount() {
+    this.setState({ theme: window.__theme })
+    window.__onThemeChange = () => {
+      this.setState({ theme: window.__theme })
+    }
+  }
+
+  setTheme(color) {
+    try {
+      localStorage.setItem('theme', color)
+      this.setState({ theme: color })
+      document.body.className = color // Not optimal we are doing this here
+    } catch (err) {}
+  }
+
   render() {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
@@ -45,7 +78,7 @@ class Layout extends React.Component {
               textDecoration: `none`,
               color: `#87C946`,
               borderBottom: `5px solid #87C946`,
-              lineHeight: `1.4`
+              lineHeight: `1.4`,
             }}
             to={`/`}
           >
@@ -70,13 +103,41 @@ class Layout extends React.Component {
     return (
       <div
         style={{
+          color: 'var(--textNormal)',
+          background: 'var(--bg)',
           marginLeft: `auto`,
           marginRight: `auto`,
           maxWidth: rhythm(24),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
-        {header}
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          {header}
+          <Toggle
+            icons={{
+              checked: (
+                <img
+                  src={moon}
+                  width="16"
+                  height="16"
+                  role="presentation"
+                  style={{ pointerEvents: 'none' }}
+                />
+              ),
+              unchecked: (
+                <img
+                  src={sun}
+                  width="16"
+                  height="16"
+                  role="presentation"
+                  style={{ pointerEvents: 'none' }}
+                />
+              ),
+            }}
+            checked={this.state.theme === 'dark'}
+            onChange={e => this.setTheme(e.target.checked ? 'dark' : 'light')}
+          />
+        </div>
         {children}
         <Footer />
       </div>
